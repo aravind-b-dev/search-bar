93575c46990a2ee1a8fb3006849dc43930ebe0bf
aravind-b-python <baravind460@gmail.com>
2021-12-09 14:31:14 +0530

    search bar added

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Serach',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Search bar'),
     );
   }
 }
@@ -31,41 +31,101 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+
+  TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){
+              showSearch(
+                  context : context,
+                  delegate: CustomSearchDelegate(),
+              );
+            },
+          )
+        ],
       ),
     );
   }
+}
+
+class CustomSearchDelegate extends SearchDelegate {
+  List<String> searchTerms = [
+    "Apple",
+    "Orange",
+    "Grape",
+    "Pineapple",
+    "Banana",
+    "Pear",
+  ];
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+   return [
+     IconButton(
+       icon: Icon(Icons.clear),
+       onPressed: (){
+         query = "";
+       },
+     )
+   ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return
+      IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: (){
+          close(context, null);
+        },
+      );
+
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for(var fruit in searchTerms){
+      if (fruit.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(fruit);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index){
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      }
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for(var fruit in searchTerms){
+      if (fruit.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(fruit);
+      }
+    }
+    return ListView.builder(
+        itemCount: matchQuery.length,
+        itemBuilder: (context, index){
+          var result = matchQuery[index];
+          return ListTile(
+            title: Text(result),
+          );
+        }
+    );
+
+  }
+
 }
